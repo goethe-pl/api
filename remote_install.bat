98b48e8fa7fc3cb6f4bcd6361938840eabe291b5
tom <tom@sapletta.com>
2020-07-02 19:55:18 +0200

    implementation of deployment

---
+++ remote_install.bat
@@ -0,0 +1,5 @@
+cd F:\PycharmProjects\faas-ovh-api\
+py deploy.py api.goethe.pl status.sh
+py deploy.py api.goethe.pl remove.sh
+py deploy.py api.goethe.pl install.sh
+cd F:\WebstormProjects\goethe-pl-api
